fb170d508463c8a33768e636f30eca1a4a7ae4f7
Matthew Rocklin <mrocklin@gmail.com>
2016-12-11 15:04:09 -0500

    update dscheduler to dask-scheduler, etc.

--- start-dask-scheduler.sh
+++ start-dask-scheduler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # XXX: instead of using the default '*' host wildcard it is highly recommended
-# to configure a public DNS name for the dscheduler service endpoint,
+# to configure a public DNS name for the dask-scheduler service endpoint,
 # preferably with a TLS certificate and define the STATUS_ENDPOINT environment
 # variable to something like "publicname:443" instead.
 STATUS_ENDPOINT="${STATUS_ENDPOINT:-"*"}"
-exec su $BASICUSER -c "env PATH=$PATH dscheduler --host dscheduler --bokeh-whitelist="$STATUS_ENDPOINT" $*"
+exec su $BASICUSER -c "env PATH=$PATH dask-scheduler --host dask-scheduler --bokeh-whitelist="$STATUS_ENDPOINT" $*"
--- start-dworker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
-echo "Detected $NCPUS cpus"
-# Alternatively start one python process per CPU
-# exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 --nthreads 1 --nprocs $NCPUS $*"
-exec su $BASICUSER -c "env PATH=$PATH dworker dscheduler:8786 $*"
