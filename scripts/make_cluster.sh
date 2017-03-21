0693e01f6aa2dcb2414ddb8505dbf828a88c447e
Martin Durant <martin.durant@utoronto.ca>
2017-03-21 15:47:25 -0400

    Working implementation

--- scripts/make_cluster.sh
+++ scripts/make_cluster.sh
@@ -1,11 +1,14 @@
+#!/usr/bin/env bash
 gcloud config set compute/zone us-east1-b
 
-gcloud container clusters create distributed-1 --num-nodes 2 -m n1-standard-4 --no-async \
-	--disk-size=50 --tags=distributed,anacondascale --enable-autoscaling --min-nodes=2 --max-nodes=3
+gcloud container clusters create distributed-1 --num-nodes 6 -m n1-standard-4 --no-async \
+	--disk-size=50 --tags=distributed,anacondascale # --enable-autoscaling --min-nodes=6 --max-nodes=16
 
 gcloud container clusters get-credentials distributed-1
 
-# gcloud container clusters resize distributed-1 5 --async
+kubectl create -f kubernetes
+
+# gcloud container clusters resize distributed-1 --size=5 --async
 # gcloud container clusters list
 
 # gcloud container clusters delete distributed-1
